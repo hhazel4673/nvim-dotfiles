5b45835b430abb8b4274fdfca99205d1f7813407
= <hhazel4673@protonmail.com>
2025-11-04 21:15:29 -0600

    fixed some of the color scheme, still needs work like a lot of work

--- colors/FuckYourEyes.lua
+++ colors/FuckYourEyes.lua
@@ -1,39 +1,34 @@
-local M = {}
+local colors = {
+    white  = '#FFFFFF',
+    bg     = '#333333',
+    fg     = '#424242',
+    red    = '#f06c6c', 
+    green  = '#6cf0b2',
+    blue   = '#7accfa', 
+    yellow = '#e7ff00', 
+    purple = '#b677f2', 
+    cyan   = '#3cb7bd', 
+    gray   = '#2b2b2b', 
+    paren  = '#00F2FF',
+}
 
-function M.setup()
+-- Highlight groups
+local highlights = {
+    Keyword = { fg = colors.cyan },
+    String = { fg = colors.green },
+    CursorLine = { bg = colors.grey },
+    StatusLine = { fg = colors.fg, bg = colors.white },
+    Function = { fg = colors.green },
 
-    local colors = {
-        bg     = '#333333',
-        fg     = '#424242',
-        red    = '#f06c6c', 
-        green  = '#6cf0b2',
-        blue   = '#7accfa', 
-        yellow = '#e7ff00', 
-        purple = '#b677f2', 
-        cyan   = '#3cb7bd', 
-        gray   = '#828282', 
-    }
+    -- Treesitter supported options
+    ["@variable"] = { fg = colors.blue },
+    ["@keyword"] = { fg = colors.cyan, bold = true, italic = true },
+    ["@function"] = { fg = colors.green },
+    ["@punctuation.bracket"] = { fg = colors.paren },
 
-    -- Highlight groups
-    local highlights = {
-        Normal = { fg = colors.fg, bg = colors.bg },
-        Comment = { fg = colors.grey },
-        Keyword = { fg = colors.cyan },
-        String = { fg = colors.green },
-        LineNr = { fg = colors.grey },
-        CursorLine = { bg = colors.cyan },
-        StatusLine = { fg = colors.fg, bg = colors.cyan },
-        -- Treesitter supported options
-        ["@variable"] = { fg = colors.blue },
-        ["@keyword"] = { fg = colors.cyan },
+}
 
-    }
-
-    -- Apply highlights
-    for group, opts in pairs(highlights) do
-        vim.api.nvim_set_hl(0, group, opts)
-    end
-    
+-- Apply highlights
+for group, opts in pairs(highlights) do
+    vim.api.nvim_set_hl(0, group, opts)
 end
-
-return M
