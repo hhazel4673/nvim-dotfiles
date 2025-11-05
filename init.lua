943ba39e5c5d8cecb5d8c776af116a9c09c0f8d8
= <hhazel4673@protonmail.com>
2025-11-05 15:52:30 -0600

    added basic framework for lsp, changed a bit of other things around in init.lua

--- init.lua
+++ init.lua
@@ -182,7 +182,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 -- Set filetype-specific settings
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup,
-  pattern = { "lua", "python", "rust" },
+  pattern = { "lua", "python", "rust", "c" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
@@ -191,7 +191,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup,
-  pattern = { "javascript", "typescript", "json", "html", "css" },
+  pattern = { "nix", "javascript", "typescript", "json", "html", "css" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
@@ -436,6 +436,159 @@ end
 vim.keymap.set('n', '<leader>bd', smart_close_buffer, { desc = 'Smart close buffer/tab' })
 
 -- ============================================================================
+-- LSP
+-- ===========================================================================
+-- Function to find project root
+local function find_root(patterns)
+    local path = vim.fn.expand('%:p:h')
+    local root = vim.fs.find(patterns, { path = path, upward = true })[1]
+    return root and vim.fn.fnamemodify(root,':h') or path
+end
+
+-- Shell LSP setup
+local function setup_shell_lsp()
+    vim.lsp.start({
+        name = 'bashls',
+        cmd = {'bash-language-server', 'start'},
+        filetypes = {'sh', 'bash', 'zsh'},
+        root_dir = find_root({'.git', 'Makefile'}),
+        settings = {
+            bashIde = {
+                blobPattern = "*@(.sh|.inc|.bash|.command)"
+            }
+        }
+    })
+end
+
+--[[
+-- Python LSP setup
+local function setup_python_lsp()
+    vim.lsp.start({
+        name = 'pylsp',
+        cmd = {'pylsp'},
+        filetypes = {'python'},
+        root_dir = find_root({'pyproject.toml', 'setup.py', setup.cfg', requirements.txt', '.git'}),
+        settings = {
+            pylsp = {
+                plugins = {
+                    pycodestyle = {
+                        enabled = false
+                    },
+                    flake8 = {
+                        enabled = true
+                    },
+                    black = {
+                        enabled = true
+                    },
+                }
+            }
+        }
+    })
+end
+]]--
+
+-- Auto-start LSPs based on filetype
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'sh,bash,zsh',
+    callback = setup_shell_lsp,
+    desc = 'Start shell LSP'
+})
+
+--[[
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'python',
+    callback = setup_python_lsp,
+    desc = 'Start python LSP'
+})
+]]--
+
+-- formatting
+local function format_code()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    local filetype = vim.bo[bufnr].filetype
+
+    local cursor_pos = vim.api.nvim_wil_get_cursor(0)
+
+    if filetype == 'python' or filename:match('%.py$') then
+        if filename == '' then
+            print("Save the file first before formatting Python")
+            return
+        end
+
+        local black_cmd = "black --quiet " .. vim.fn.shellescape(filename)
+        local black_result = vim.fn.system(black_cmd)
+
+        if vim.v.shell_error == 0 then
+            vim.cmd('checktime')
+            vim.api.nvim_win_set_cursor(0, cursor_pos)
+            print("Formatted with black")
+            return
+        else
+            print("No python formatter available (install black)")
+            return
+        end
+    end
+
+    if filetype == 'sh' or filetype == 'bash' or filename:match('%.sh$') then
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        local conftent = table.concat(lines, '\n')
+
+        local cmd = {'shfmt', '-i', '2', '-ci', '-sr'} -- 2 spaces, case indent, space redirects
+        local result = vim.fn.system(cmd, content)
+
+        if vim.v.shell_error == 0 then
+            local formatted_lines = vim.spilt(result, '\n')
+            if formatted_lines[#formatted_lines] == '' then
+                table.remove(formatted_lines)
+            end
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, formatted_lines)
+            vim.api.nvim_win_set_cursor(0, cursor_pos)
+            print("Shell script formatted with shfmt")
+            return
+        else
+            print("shfmt error: " .. result)
+            return
+        end
+    end
+
+    print("no formatter available for " .. filetype)
+end
+
+vim.api.nvim_create_user_command("FormatCode", format_code, {
+    desc = "format current file"
+})
+
+vim.keymap.set('n', '<leader>fm', format_code, { desc = 'Format file' })
+
+-- LSP keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(event)
+        local opts = {buffer = event.buf}
+
+        -- Navigation
+        vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gs', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+
+        -- Information
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+        -- Code actions
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+
+        -- Diagnostics
+        vim.keymap.set('n', '<leader>nd', vim.diagnostic,goto_next, opts)
+        vim.keymap.set('n', '<leader>pd', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+    end,
+})
+
+-- ============================================================================
 -- STATUSLINE
 -- ============================================================================
 
@@ -513,7 +666,23 @@ local function mode_icon()
     ["!"] = "[SH]",
     t = "[TER]"
   }
-return modes[mode] or "  " .. mode:upper()
+  local modeFinal = modes[mode] or "[UNKNOWN]"
+
+  -- highlights on the mode thing are hard to do
+  --[[
+  local hl = "Other"
+  if modeFinal:find "[NAV]" then
+      hl = "Normal"
+  elseif modeFinal:find "[INS]" then
+      hl = "Insert"
+  elseif modeFinal:find "[VIS]" or mode:find "[V-LINE]" or mode:find "[V-BLOCK]" then
+      hl = "Visual"
+  elseif modeFinal:find "[CMD]" then
+      hl = "Command"
+  end
+  ]]--
+
+return modeFinal -- string.format("%s%s", hl, modeFinal)
 end
 
 _G.mode_icon = mode_icon
